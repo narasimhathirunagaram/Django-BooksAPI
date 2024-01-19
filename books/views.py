b37bef5b63e6d7320192e8e3df43e379cd9b63f1
Narasimha Thirunagaram <tnarasimha214@gmail.com>
2024-01-19 13:44:24 +0530

    POST method modified

--- books/views.py
+++ books/views.py
@@ -1,7 +1,11 @@
 from .models import Book
 from django.shortcuts import render, redirect
 from .forms import BookForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+import json
 
+@csrf_exempt
 def book_list(request):
     # GET method - retrieve the list of books
     books = Book.objects.all()
@@ -9,9 +13,15 @@ def book_list(request):
 
     # POST method - handle form submission to add a new book
     if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('book-list')
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            form = BookForm(data)
+            if form.is_valid():
+                form.save()
+                return JsonResponse({'success': True})
+            else:
+                return JsonResponse({'errors': form.errors}, status=400)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON data'}, status=400)
 
     return render(request, 'books_list.html', {'books': books, 'form': form})
